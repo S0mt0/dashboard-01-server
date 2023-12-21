38f81f04f82e52f9b57afea6a18bc17c6d0d9de9
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-12-21 04:52:00 +0100

    fixed script to run app through the correct app entry file

--- src/setup/config/cors/index.ts
+++ src/setup/config/cors/index.ts
@@ -6,20 +6,17 @@ const prodOrigin = [
 ];
 
 const devOrigin = [
-  "http://localhost:3001",
   "http://localhost:3000",
   "http://localhost:5173",
   "https://dhlimited.vercel.app",
   "https://www.dhlimited.vercel.app",
   "https://dhlimited.onrender.com",
   "https://www.dhlimited.onrender.com",
-  "https://toshtracker.onrender.com",
-  "https://www.toshtracker.onrender.com",
   "[::1]:3001",
   "[::1]:3000",
 ];
 
-const allowedOrigin =
+const allowedOrigins =
   process.env.NODE_ENV === "production" ? prodOrigin : devOrigin;
 
 /**
@@ -39,7 +36,7 @@ export const corsOptions: CorsOptions = {
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   origin: (origin, callback) => {
-    if (devOrigin.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS..."));
