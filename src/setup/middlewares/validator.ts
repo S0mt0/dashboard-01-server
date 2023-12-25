79932961385e6f41bfac6ac4da5213dde0de5647
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-12-25 09:23:04 +0100

    test

--- src/setup/middlewares/validator.ts
+++ src/setup/middlewares/validator.ts
@@ -11,6 +11,8 @@ export const validate =
     const schema = Joi.object().keys(fields).required().unknown(false);
 
     const payload = req.body;
+    console.log("[REQ.BODY]: ", req.body);
+
     const { error, value } = schema.validate(payload, {
       abortEarly: false,
     });
