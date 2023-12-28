2627d36daf381824b7a2ccabeef79f219d956da8
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-12-28 15:48:45 +0100

    mail template update

--- dist/sdk/lib/mails/templates/password-reset/mail-content/index.js
+++ dist/sdk/lib/mails/templates/password-reset/mail-content/index.js
@@ -55,116 +55,107 @@ const resetPasswordMailContent = (payload) => {
     return {
         text: `Dear ${payload.username}, you requested to reset your password on Afrolay. Use the 6-digit token below to initiate a password reset.`,
         html: `<html lang="en">
-          <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <style>
-              *,
-              * > * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                color: #5a5858;
-              }
+            <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <style>
+                *,
+                * > * {
+                  margin: 0;
+                  padding: 0;
+                  box-sizing: border-box;
+                  color: #5a5858;
+                }
 
-              .container {
-                background: #f0efefc7;
-                padding-inline: 1rem;
-              }
+                .container {
+                  padding-inline: 1rem;
+                }
 
-              main {
-                width: 90%;
-                background: white;
-                margin: 0 auto;
-                max-width: 620px;
-                background-color: #fff
+                main {
+                  width: 90%;
+                  margin-inline: auto;
+                  max-width: 620px;
+                }
 
-              }
+                header {
+                  padding: 2rem;
+                  border-bottom: 1px solid #f7b946d7;
+                  text-align-center
+                }
 
-              header {
-                padding: 2rem;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-bottom: 1px solid #f7b946d7;
-                background-color: #fff
-              }
+                header img {
+                  width: 150px;
+                  display: inline;
+                }
 
-              header img {
-                width: 150px;
-              }
+                .body {
+                  padding: 2rem 1.1rem;
+                }
 
-              .body {
-                padding: 2rem 1.1rem;
-                background-color: #fff
+                h1 {
+                  font-size: 1rem;
+                  margin-bottom: 1rem;
+                  color: #000;
+                }
 
-              }
+                h3 {
+                  margin-bottom: 1rem;
+                  margin-top: 1rem;
+                  color: #000;
+                }
 
-              h1 {
-                font-size: 1rem;
-                margin-bottom: 1rem;
-                color: #000;
-              }
+                .ps {
+                  text-align: center;
+                  width: 80%;
+                  margin: 1rem auto;
+                  padding: 1rem;
+                }
 
-              h3 {
-                margin-block: 1rem;
-                color: #000;
-              }
+                .ps small {
+                  color: #b1b0b0;
+                  font-size: 11px;
+                }
 
-              .ps {
-                text-align: center;
-                width: 80%;
-                margin: 1rem auto;
-                padding: 1rem;
-              }
+                @media screen and (min-width: 768px) {
+                .body, 
+                header, 
+                main,
+                .container {
+                  background-color: #fff
+                  }
+                }
+              </style>
+            </head>
+            <body>
+              <div class="container">
+                <main>
+                  <header>
+                    <img
+                      src="https://res.cloudinary.com/doszbexiw/image/upload/v1703759856/Afrolay/logo-01_cjdnpu.png"
+                      alt="logo"
+                    />
+                  </header>
+                  <div class="body">
+                    <h1>Dear ${payload.username},</h1>
+                    <p>
+                      You requested to reset your password on
+                      <strong>Afrolay&trade;.</strong> Use the 6-digit code below to
+                      initiate a password reset.
+                    </p>
+                    <h3>${payload.otp}</h3>
+                    <p>This code is valid for the next 15 minutes.</p>
+                  </div>
 
-              .ps small {
-                color: #b1b0b0;
-                line-height: 1.1;
-                font-size: 11px;
-              }
-            </style>
-          </head>
-          <body>
-            <div class="container">
-              <main style="
-              background-color: #fff;
-              width: 90%;
-              margin: 0 auto;
-              max-width: 620px;
-              ">
-                <header style="
-                background-color: #fff;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                ">
-                  <img
-                    src="https://res.cloudinary.com/doszbexiw/image/upload/v1703759856/Afrolay/logo-01_cjdnpu.png"
-                    alt="logo"
-                  />
-                </header>
-                <div class="body" style="background-color: #fff">
-                  <h1>Dear ${payload.username},</h1>
-                  <p>
-                    You requested to reset your password on
-                    <strong>Afrolay&trade;.</strong> Use the 6-digit code below to
-                    initiate a password reset.
-                  </p>
-                  <h3 style="margin-block: 1rem">${payload.otp}</h3>
-                  <p>This code is valid for the next 15 minutes.</p>
-                </div>
-
-                <div class="ps">
-                  <small
-                    >This message was intended for ${payload.username}. If you did not request a
-                    password reset from Afrolay&trade;, please ignore this email.</small
-                  >
-                </div>
-              </main>
-            </div>
-          </body>
-        </html>`,
+                  <div class="ps">
+                    <small
+                      >This message was intended for ${payload.username}. If you did not request a
+                      password reset from Afrolay&trade;, please ignore this email.</small
+                    >
+                  </div>
+                </main>
+              </div>
+            </body>
+          </html>`,
     };
 };
 exports.resetPasswordMailContent = resetPasswordMailContent;
